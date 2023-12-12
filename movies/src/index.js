82cc453312d86b3bdf03a9d798009b0b78eedbea
Sachin Saji <sachinsaji71@gmail.com>
2023-12-12 13:19:30 +0530

    final commit

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,7 +10,7 @@ import { store } from './store';
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL="https://movie-book-app-ss.onrender.com/"
+axios.defaults.baseURL="https://movie-book-app-ss.onrender.com"
 root.render(
   <React.StrictMode>
   <BrowserRouter>
